8148da5abbf49d2486ca84f7c41936c4ed86c981
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-07-19 21:30:44 +0800

    play around with gatsby js

---
+++ src/pages/page-2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+  <div>
+    <p>Hello world from my second page</p>
+    <Link to="/">back home</Link>
+  </div>
+);
