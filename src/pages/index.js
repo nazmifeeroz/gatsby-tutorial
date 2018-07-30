f39ca3946aa42c26fb32ba8ec11f3870b6d47e77
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-07-30 21:24:17 +0800

    write some css code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,26 @@
-import React from "react";
-import Link from "gatsby-link";
+import React from 'react'
 
 export default () => (
-  <div style={{ color: "tomato" }}>
-    <h1>Hello world!</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-    <br />
+  <div style={{ margin: '3rem auto', maxWidth: 600 }}>
+    <h1>Nazmi on Gatsby!</h1>
     <div>
-      <Link to="/page-2/">Link</Link>
+      <p>
+        From Nazmi's class and must-read article, "<a href="https://medium.com/@nazmifeeroz/my-journey-to-be-a-full-stack-web-developer-week-3-d416e821679d">
+          My journey to be a developer
+        </a>".
+      </p>
+      <blockquote>
+        <p>
+          There is indeed an element of luck, and no, there isn't. The prepared
+          mind sooned or later finds something important and does it. So yes, it
+          is luck.{' '}
+          <em>
+            The particular thing you do is luck, but that you do something is
+            not.
+          </em>
+        </p>
+      </blockquote>
     </div>
-    <div>
-      <Link to="/counter/">Counter</Link>
-    </div>
+    <p>Posted July 30, 2018</p>
   </div>
-);
+)
